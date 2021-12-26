b4613bac5a22eacea3bfcf5757944e81866d4dbf
Robert Laws <roblaws@me.com>
2021-12-26 15:20:36 +0300

    add forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,50 @@
+import { useState } from 'react';
+
 export const Login = () => {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(login);
+  };
+
   return (
     <main className='layout-960'>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit} className='form'>
+        <label>
+          <span>email:</span>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={login.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span>password:</span>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            value={login.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button className='btn'>Login</button>
+      </form>
     </main>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,61 @@
+import { useState } from 'react';
+
 export const Signup = () => {
+  const [signup, setSignup] = useState({
+    displayName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setSignup({
+      ...signup,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(signup);
+  };
+
   return (
     <main className='layout-960'>
       <h1>Signup</h1>
+      <form onSubmit={handleSubmit} className='form'>
+        <label>
+          <span>display name:</span>
+          <input
+            id='displayName'
+            type='text'
+            name='displayName'
+            value={signup.displayName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span>email:</span>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={signup.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span>password:</span>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            value={signup.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button className='btn'>Sign Up</button>
+      </form>
     </main>
   );
 };
